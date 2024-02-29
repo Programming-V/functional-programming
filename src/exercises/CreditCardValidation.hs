94531f8180254430f77a9ae94875eeeff9d8c142
José Luis Terán <joseluisteran2617@gmail.com>
2024-02-29 16:26:38 -0400

    feat(toDigitsRev): add function to convert Integer to a reversed list

--- src/exercises/CreditCardValidation.hs
+++ src/exercises/CreditCardValidation.hs
@@ -3,3 +3,8 @@ module Exercises where
 toDigits :: Integer -> [Integer]
 toDigits d | d <= 0 = []
            | otherwise = toDigits (d `div` 10) ++ [d `mod` 10]
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev d | d <= 0 = []
+              | otherwise = d `mod` 10 : toDigits (d `div` 10)
+  
