5d3b145726ae53e93f0a358ffc9e9fcd28b0b076
José Luis Terán <joseluisteran2617@gmail.com>
2024-03-04 16:36:54 -0400

    feat(5): implement response for analysis question

--- src/chapters/Chapter03.hs
+++ src/chapters/Chapter03.hs
@@ -31,3 +31,19 @@ twice f x = f (f x)
 -- 4. Check your answers to the preceding three questions using GHCi.
 
 -- All the types written above have been compiled.
+
+{-
+  5. Why is it not feasible in general for function types to be instances of the Eq class? 
+  When is it feasible? 
+  Hint: two functions of the same type are equal if they always return equal 
+  results for equal arguments.
+-}
+
+{-
+  It's not feasible due to there is no general algorithm that can decide whether an arbitrary 
+  program will halt or continue running indefinitely, leading to Halting Problem.
+  On the other hand, it can be feasible when a function's output only depends on its inputs, 
+  and there are no side effects (pure functions), which are transparent and has the same 
+  output for the same input, so it can be consider them equal for the purposes of equality 
+  comparison.
+-}
